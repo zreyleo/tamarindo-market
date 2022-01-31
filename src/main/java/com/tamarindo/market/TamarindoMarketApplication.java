a1a0e77559a4dcbc5827b9f01c852bcb7d568071
Regynald Zambrano <zreyleo@gmail.com>
2022-01-30 20:20:24 -0500

    conexion a la base de datos

---
+++ src/main/java/com/tamarindo/market/TamarindoMarketApplication.java
@@ -0,0 +1,13 @@
+package com.tamarindo.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TamarindoMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TamarindoMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tamarindo/market/TamarindoMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamarindo.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TamarindoMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
