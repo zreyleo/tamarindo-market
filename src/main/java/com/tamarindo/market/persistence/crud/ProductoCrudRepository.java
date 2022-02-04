a6e7fa3d22ac0ba013932c1fcd1e7b0b222348ae
Regynald Zambrano <zreyleo@gmail.com>
2022-02-04 10:31:56 -0500

    clase-20: anotacion Repository

---
+++ src/main/java/com/tamarindo/market/persistence/crud/ProductoCrudRepository.java
@@ -0,0 +1,15 @@
+package com.tamarindo.market.persistence.crud;
+
+import com.tamarindo.market.persistence.entity.Producto;
+// import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+    // @Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true)
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidad, boolean estado);
+}
